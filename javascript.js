9da24afe4426a59018859b9efbd3a3cff5d88ca4
luke <lukeerosa@gmail.com>
2025-07-21 18:41:03 +0200

    js modified

--- javascript.js
+++ javascript.js
@@ -7,10 +7,20 @@ function getComputerChoice(){
     return "paper"
  } else if (number === 2){
     return "scissor"
-
- }
-
-
+}
 }
 
 console.log(getComputerChoice())
+// funzione per la persona che sceglie
+function getHumanChoice(){
+    let answer = prompt("Choose: Rock Paper Scissors");
+   
+    if(answer == "rock"){
+        return "rock";
+    } else if (answer == "paper"){
+        return "paper";
+    } else if (answer == "scissors"){
+        return "scissor";
+    }
+}
+console.log(getHumanChoice())

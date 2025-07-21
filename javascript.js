00d2493ec67b27f7cd0b0c86b88826d4c153bd5e
luke <lukeerosa@gmail.com>
2025-07-21 18:24:36 +0200

    js modified

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("ciao")
+//prima funzione per la scelta del computer
+function getComputerChoice(){
+ let number = Math.floor(Math.random()*3);
+ if(number === 0){
+    return "rock"
+ } else if (number === 1){
+    return "paper"
+ } else if (number === 2){
+    return "scissor"
+
+ }
+
+
+}
+
+console.log(getComputerChoice())
